903a008d88f6befa07d4df752da3c2403ce96c4d
Nur Majid Nor Kalam <147082233+MajidInFocus@users.noreply.github.com>
2024-12-27 06:27:41 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+package org.schoolFinancialSystem;
+
+public class Main {
+    //TODO: Call login class
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+
+    }
+}
